87254d4f701f7cb8468e8dc67542fa673fb8b856
Rohan Prasad <studentrrp@gmail.com>
2020-10-27 12:40:54 +0530

    Added delete with removing without refresh

--- client/src/components/GetData.js
+++ client/src/components/GetData.js
@@ -12,31 +12,62 @@ class GetData extends Component {
         const userObject = {
             name: 'AddData',
         };
-        try {
-            axios.post('http://localhost:5000/api/items',userObject)
-            .then(console.log("added Item"))
-        }catch{
-            console.log("Error2")   
-
+        
         // try {
-        //     API.get()
-        //     .then(res => {
-        //         console.log("Got a response")
-        //         console.log(res.data[0]._id)
-        //         axios.delete(`http://localhost:5000/api/items/${res.data[0]._id}`)
-        //         .then(res =>{
-        //             console.log(res.data)
-        //         })
-        //         this.setState({})
-        //     })
+        //     axios.post('http://localhost:5000/api/items',userObject)
+        //     .then(
+        //             console.log("added Item")
+        //             //this.setState({data:res.data[0]})
+        //          )
+            
         // }catch{
-        //     console.log("Error")
-        // }
-        //
+        //     console.log("Error2")   
+
+        
+            try {
+                API.get()
+                .then(res => {
+                    console.log("Got a response")
+                    console.log(res.data[0]._id)
+                    // axios.delete(`http://localhost:5000/api/items/${res.data[0]._id}`)
+                    // .then(res =>{
+                    //     console.log(res.data)
+                    // })
+                   this.setState({data :res.data})
+                })
+            }catch{
+                console.log("Error")
+            }
         
     }
-}
+
+    onClick = (e) =>{
+        const del = e.currentTarget.parentNode.id
+        console.log(e.currentTarget.parentNode.id)
+        axios.delete(`http://localhost:5000/api/items/${del}`)
+                    .then(res =>{
+                        console.log(res.data)
+                    })
+        const chg = this.state.data.filter(e =>(
+            e._id !== del
+        ))
+        this.setState({
+            data : chg
+        })
+    }
     render() { 
+        if(this.state.data){
+            return (
+                <ul>
+                   {this.state.data.map(e => (
+                       <div key={e._id} id={e._id}  >
+                           <button onClick={this.onClick}>Del</button>
+                       </div>
+                   ))}
+                </ul>
+            )
+            return <> Hello</>
+        }
         return ( <div>
             {this.state.data}
             HH
